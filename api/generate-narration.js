d96fec3f6e2b5f05e2c772d204c7d4746a8a3ed9
horiikehisako-prog <horiikehisako-prog@users.noreply.github.com>
2026-07-09 14:29:20 +0900

    allow external ai proxy calls

--- api/generate-narration.js
+++ api/generate-narration.js
@@ -338,6 +338,15 @@ const requestNarration = async ({ apiKey, model, temperature, maxTokens, prompt,
 
 module.exports = async (req, res) => {
   res.setHeader("Content-Type", "application/json; charset=utf-8");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.statusCode = 204;
+    res.end();
+    return;
+  }
 
   if (req.method !== "POST") {
     res.statusCode = 405;
